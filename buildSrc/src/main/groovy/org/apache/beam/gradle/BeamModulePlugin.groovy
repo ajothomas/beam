82fa1dec7cc63b4388f1bfa9be5f91e451833a68
Ajo Thomas <athomas1@godaddy.com>
2019-06-05 09:51:31 -0700

    Changing the release version

--- buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
+++ buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
@@ -286,7 +286,7 @@ class BeamModulePlugin implements Plugin<Project> {
 
     // Automatically use the official release version if we are performing a release
     // otherwise append '-SNAPSHOT'
-    project.version = '2.10.0gd6'
+    project.version = '2.10.0gd10'
     if (!isRelease(project)) {
       project.version += '-SNAPSHOT'
     }
